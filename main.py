74924b7417efcc69819a9b8a267ec96bc0aefedf
Huy Tu <tranhuytu242000@gmail.com>
2022-04-09 22:09:57 +0700

    test

--- main.py
+++ main.py
@@ -57,10 +57,10 @@ def connect_and_login(group_id: str):
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_argument("--disable-notifications")
     options.add_argument("--headless")
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--start-maximized")
+    options.add_argument('--disable-gpu')
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("window-size=1400,900")
     chrome = webdriver.Chrome(executable_path=os.environ.get(
         "CHROMEDRIVER_PATH"), chrome_options=options)
 
